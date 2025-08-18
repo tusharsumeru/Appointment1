01ec72f268547c10ee166b466d81ff47e00f7a1c
shreya jaganur <shreyajaganur@gmail.com>
2025-08-18 10:07:25 +0530

    updated

--- lib/action/action.dart
+++ lib/action/action.dart
@@ -11,7 +11,7 @@ import 'jwt_utils.dart'; // Added import for JwtUtils
 class ActionService {
   static const String baseUrl =
       // API base URL
-      'https://43d959415590.ngrok-free.app/api/v3'; // API base URL
+      'https://1d79ae411e26.ngrok-free.app/api/v3'; // API base URL
 
   static Future<Map<String, dynamic>> getAllSecretaries({
     int page = 1,
